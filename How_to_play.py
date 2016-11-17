35cb022e0ab8b03e50051ed9a2d226db62108eb6
Lee-Youn-Sang <eys920604@naver.com>
2016-11-17 23:31:46 +0900

    Add files via upload

---
+++ How_to_play.py
@@ -0,0 +1,45 @@
+import game_framework
+import second_logo
+import main_state
+from pico2d import *
+
+name = "How"
+
+image = None
+
+def enter():
+    global image
+    image = load_image('images\\Logo\\How to play.png')
+
+def exit():
+    pass
+
+def update():
+   pass
+
+def draw():
+    global image
+    clear_canvas()
+    image.draw(400, 300)
+    update_canvas()
+
+def handle_events():
+    global x,y
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.change_state(second_logo)
+
+        if event.type == SDL_MOUSEMOTION:
+            x,y = event.x, 600 - event.y
+        if event.type == SDL_MOUSEBUTTONDOWN and event.button ==  SDL_BUTTON_LEFT:
+            if 720< x and x <840 and 40<y and y<90:
+                game_framework.change_state(second_logo)
+
+def pause():
+    pass
+
+def resume():
+    pass
